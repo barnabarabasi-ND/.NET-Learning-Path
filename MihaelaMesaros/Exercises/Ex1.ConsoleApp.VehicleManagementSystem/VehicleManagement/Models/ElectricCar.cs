f6ab387368f12f0ce512236f5732a10382ef59fa
Mihaela Mesaros <mihaela.mesaros@endava.com>
2026-07-28 10:06:10 +0300

    Practical Exercises 1 - Example 1 - Vehicle Management System

---
+++ MihaelaMesaros/Exercises/Ex1.ConsoleApp.VehicleManagementSystem/VehicleManagement/Models/ElectricCar.cs
@@ -0,0 +1,13 @@
+﻿
+namespace VehicleManagement.Models
+{
+    public class ElecticCar : Car
+    {
+		public int BatteryRange { get; set; }
+
+		public override void StartEngine()
+		{
+			Console.WriteLine($"The electric car {this.Brand} {this.Model} starts from button.");
+		}
+	}
+}

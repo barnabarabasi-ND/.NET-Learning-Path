4c29bcaaa9c3f26ce0d7266ab7065a0cdcf470e3
Balint Zoltan <zzoolliikkaa@gmail.com>
2026-08-04 11:25:25 +0300

    DEmo source code for Multithreading and Async prog topic.

---
+++ BalintZoltan/Multithreading_and_Asynchronous_Programming/Synchronization/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+[DllImport("Kernel32.dll")]
+
+static extern int GetCurrentThreadId();
+HashSet<int> threadID = new HashSet<int>();
+object obj = new();
+
+Console.WriteLine("Hello, World!");
+int counter = 0;
+
+Parallel.For(0, 100000, i =>
+{
+// With lock
+    //lock (obj)
+    //{
+    //    counter++;
+    //    threadID.Add(GetCurrentThreadId());
+    //}
+
+// Without lock
+
+    //counter++;
+    //threadID.Add(GetCurrentThreadId());
+
+//Interlocked
+    Interlocked.Increment(ref counter);                                     // Only simple execution , Not for : threadID.Add(GetCurrentThreadId());
+});
+Console.WriteLine("Number of thread : " + threadID.Count());
+Console.WriteLine(counter);
---
+++ BalintZoltan/Multithreading_and_Asynchronous_Programming/Task/Program.cs
@@ -0,0 +1,30 @@
+﻿Task<int> task = Task.Run(() =>
+{
+    Console.WriteLine("Working...");
+    Thread.Sleep(2000);
+    return 0;
+});
+
+int value = task.Result;                    // Blocking !!!
+Console.WriteLine("Task returned value: "+value);
+
+task.Wait();                                // Blocking !!!
+
+
+int result = await Task.Run(() =>
+{
+    Thread.Sleep(5000);
+    return 42;
+});
+Console.WriteLine("Task returned value: " + result);
+
+Example();
+
+async Task Example()
+{
+    Console.WriteLine("A");
+
+    await Task.Delay(3000);
+
+    Console.WriteLine("B");
+}

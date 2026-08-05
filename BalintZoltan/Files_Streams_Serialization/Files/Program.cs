9539f2da89b3cbb577749211da5974bf986ec406
Balint Zoltan <zzoolliikkaa@gmail.com>
2026-08-05 09:26:30 +0300

    Add demo file for Files Stream and Serialization

---
+++ BalintZoltan/Files_Streams_Serialization/Files/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Hello, World!");
+
+//File
+File.WriteAllText("test.txt", "Hello, New file!");
+var text = File.ReadAllText("test.txt");
+Console.WriteLine(text);
+File.Delete("test.txt");
+Console.WriteLine("New file deleted.");
+try
+{
+    text = File.ReadAllText("test.txt");
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine("[EX] File not found!");
+}
+if (!File.Exists("test.txt"))
+{
+    Console.WriteLine("File not found!");
+}
+
+// Directory
+Directory.CreateDirectory("Data");
+Console.WriteLine("Hello, Data folder!");
+Console.WriteLine("Data folder exists : " + Directory.Exists("Data"));
+Directory.Delete("Data");
+Console.WriteLine("Data folder deleted.");
+Console.WriteLine("Data folder exists : "+Directory.Exists("Data"));
+
+string temp = Path.GetTempPath();
+Console.WriteLine("Temp folder: "+ temp);
+
+// Path
+string path = Path.Combine(temp,"First", "file.txt");
+Console.WriteLine("Temp folder with file: " + path);
+Console.WriteLine("File name : " + Path.GetFileName(path));
+Console.WriteLine("File name without extension : " + Path.GetFileNameWithoutExtension(path));
+Console.WriteLine("File extension : " + Path.GetExtension(path));
+Console.WriteLine("Directory name : " + Path.GetDirectoryName(path));
+
+
---
+++ BalintZoltan/Files_Streams_Serialization/Stream/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("Hello, World!");
+
+File.WriteAllText("test.txt", "Hello New File");
+
+// FileStream
+// Using . . . 
+using (FileStream fs =
+    new FileStream(
+        "test.txt",
+        FileMode.Open))
+{
+    byte[] buffer = new byte[100];
+    int count = fs.Read(buffer, 0, buffer.Length);
+    Console.WriteLine("The file contain " + count + " byte(s)");
+}
+//fs.Close();                                                           // Using automatically close the unused resource.
+File.Delete("test.txt");
+
+using StreamWriter writer =
+    new StreamWriter("data.txt");
+writer.WriteLine("Hello");
+writer.WriteLine("World");
+writer.Close();                                                         // Need Close() because the using part is not finished before next using
+                                                                        // The stream remain opened.
+
+using StreamReader reader =
+    new StreamReader("data.txt");
+string text = reader.ReadToEnd();
+Console.WriteLine("The file contain: " + text);
+reader.Close();                                                         // Need Close() because the using part is not finished before next using
+                                                                        // The stream remain opened.
+
+File.Delete("data.txt");
+
+
+// MemoryStream
+Console.WriteLine("MemoryStream");
+Console.WriteLine("------------");
+Console.WriteLine();
+
+using MemoryStream ms = new MemoryStream();
+
+byte[] data = { 1, 2, 3, 4, 5 };
+
+ms.Write(data, 0, data.Length);
+
+Console.Write("The MemoryStream contain:");
+ms.Position = 0;
+// OR ms.Seek(0, SeekOrigin.Begin);
+foreach (byte b in ms.ToArray())
+{
+    Console.Write($"{b} ");
+}
+Console.Write("The MemoryStream contain:");
+for (int i = 0;i<ms.Length/2+1;i++)
+{
+    Console.Write($"{ms.ReadByte()} ");
+    ms.Seek(ms.Position+1, SeekOrigin.Begin);
+    //ms.Seek(1, SeekOrigin.Current);
+}

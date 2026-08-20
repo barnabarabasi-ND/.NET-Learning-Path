4803713c1a483196c3ffe8ca841580284c50b7c0
Balint Zoltan <zzoolliikkaa@gmail.com>
2026-08-20 14:21:26 +0300

    Add demo files for Unit testing.

---
+++ BalintZoltan/Unit_Testing/Unit/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+public class BankAccount
+{
+    public decimal Balance { get; private set; }
+
+    public BankAccount(decimal balance)
+    {
+        Balance = balance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+    public void Withdraw(decimal amount)
+    {
+        if (Balance >= amount)
+        {  
+            Balance -= amount;
+        }
+    }
+}
+
---
+++ BalintZoltan/Unit_Testing/Unit_Test/Program.cs
@@ -0,0 +1,58 @@
+﻿
+[TestClass]
+public class CalculatorTests
+{
+    [TestMethod]
+    public void Add_2And3_Returns5()
+    {
+        // Arrange
+        Calculator calc = new Calculator();
+
+        // Act
+        int result = calc.Add(2, 3);
+
+        // Assert
+        Assert.AreEqual(5, result);
+    }
+    [TestMethod]
+    public void Add_2And3_Returns6()
+    {
+        // Arrange
+        Calculator calc = new Calculator();
+
+        // Act
+        int result = calc.Add(2, 3);
+
+        // Assert
+        Assert.AreNotEqual(6, result);
+    }
+}
+
+
+[TestClass]
+public class BankAccountTests
+{
+    private BankAccount? account;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        account = new BankAccount(500);
+    }
+
+    [TestMethod]
+    public void DepositTest()
+    {
+        account.Deposit(100);
+
+        Assert.AreEqual(600, account.Balance);
+    }
+
+    [TestMethod]
+    public void WithdrawTest()
+    {
+        account.Withdraw(100);
+
+        Assert.AreEqual(400, account.Balance);
+    }
+}

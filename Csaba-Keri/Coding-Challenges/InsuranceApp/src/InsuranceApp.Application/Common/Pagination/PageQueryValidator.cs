1da3eb6784ff4e7252efcc621e9cd06e3c570f08
Csaba Keri <Csaba.Keri@endava.com>
2026-09-18 20:09:07 +0300

    Clarify pagination validation messages

--- Csaba-Keri/Coding-Challenges/InsuranceApp/src/InsuranceApp.Application/Common/Pagination/PageQueryValidator.cs
+++ Csaba-Keri/Coding-Challenges/InsuranceApp/src/InsuranceApp.Application/Common/Pagination/PageQueryValidator.cs
@@ -7,9 +7,10 @@ public class PageQueryValidator : AbstractValidator<PageQuery>
     public PageQueryValidator()
     {
         RuleFor(query => query.PageNumber)
-            .GreaterThanOrEqualTo(1);
+            .GreaterThanOrEqualTo(1).WithMessage("Page number must be at least 1.");
 
         RuleFor(query => query.PageSize)
-            .InclusiveBetween(1, PaginationDefaults.MaxPageSize);
+            .InclusiveBetween(1, PaginationDefaults.MaxPageSize)
+            .WithMessage($"Page size must be between 1 and {PaginationDefaults.MaxPageSize}.");
     }
 }

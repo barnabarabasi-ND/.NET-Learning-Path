20ff45890208834a748ee40e0f57d66b1c3c911a
Balint Zoltan <zzoolliikkaa@gmail.com>
2026-09-22 19:42:18 +0300

    fix: use named routes for CreatedAt responses

--- BalintZoltan/Insurance_App/Insurance.Api/Controllers/BuildingsController.cs
+++ BalintZoltan/Insurance_App/Insurance.Api/Controllers/BuildingsController.cs
@@ -16,7 +16,7 @@ public class BuildingsController : ControllerBase
         _buildingService = buildingService;
     }
 
-    [HttpGet("buildings/{buildingId:guid}")]
+    [HttpGet("buildings/{buildingId:guid}", Name = nameof(GetBuildingByIdAsync))]
     public async Task<ActionResult<BuildingDto>> GetBuildingByIdAsync(Guid buildingId, CancellationToken cancellationToken)
     {
         var building = await _buildingService.GetBuildingByIdAsync(buildingId, cancellationToken);
@@ -53,8 +53,8 @@ public class BuildingsController : ControllerBase
         request.ClientId = clientId;
         var building = await _buildingService.CreateBuildingAsync(request, cancellationToken);
 
-        return CreatedAtAction(
-            "GetBuildingById",
+        return CreatedAtRoute(
+            nameof(GetBuildingByIdAsync),
             new { buildingId = building.Id },
             building);
     }

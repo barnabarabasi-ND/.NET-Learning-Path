3f22670dc182a4f9aeeabab670d8ad413a769e68
Balint Zoltan <zzoolliikkaa@gmail.com>
2026-09-23 08:04:19 +0300

    fix: Email parameter to email

--- BalintZoltan/Insurance_App/Insurance.Application/Services/ClientService.cs
+++ BalintZoltan/Insurance_App/Insurance.Application/Services/ClientService.cs
@@ -148,16 +148,16 @@ public class ClientService : IClientService
         }
     }
 
-    private static void ValidateEmail(string Email)
+    private static void ValidateEmail(string email)
     {
-        if (string.IsNullOrWhiteSpace(Email))
+        if (string.IsNullOrWhiteSpace(email))
         {
             return;
         }
 
-        var email = Email.Trim();
+        var emailTrim = email.Trim();
 
-        if (email.Length > 254)
+        if (emailTrim.Length > 254)
         {
             throw new ArgumentException(
                 "Email cannot be longer than 254 characters.");
@@ -165,7 +165,7 @@ public class ClientService : IClientService
 
         var emailAttribute = new EmailAddressAttribute();
 
-        if (!emailAttribute.IsValid(email))
+        if (!emailAttribute.IsValid(emailTrim))
         {
             throw new ArgumentException(
                 "Invalid email address format.");
